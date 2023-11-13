fe47449b6dda85c45003fa2dcf38e8a6ede60b4d
WilsonVinson <cprewilsonvinson@gmail.com>
2023-11-13 11:18:30 +0800

    main

--- project/source/utility/global.gd
+++ project/source/utility/global.gd
@@ -2,7 +2,7 @@
 extends Node
 
 func _ready():
-	DisplayServer.window_set_title("花生迷途 Peanut's Journey")
+	DisplayServer.window_set_title("TYTS02")
 	
 	Engine.set_max_fps(60)
 	DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_ENABLED)
